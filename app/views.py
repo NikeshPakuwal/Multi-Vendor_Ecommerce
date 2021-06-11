f8fa1e51062eaf163e2f8f6dac7cb8bdf283b3a1
NIKESH PAKUWAL <pakuwalnikesh@gmail.com>
2021-06-11 16:11:29 +0545

    Product Details Added

--- app/views.py
+++ app/views.py
@@ -20,11 +20,17 @@ class ProductView(View):
         return render(request, 'app/home.html', context)
 
 
-def product_detail(request):
-    return render(request, 'app/productdetail.html')
+# def product_detail(request):
+#     return render(request, 'app/productdetail.html')
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk = pk)
+        return render(request, 'app/productdetail.html', {'product' : product})
+
 
 def add_to_cart(request):
-    return render(request, 'app/addtocart.html')
+     return render(request, 'app/addtocart.html')
 
 def buy_now(request):
     return render(request, 'app/buynow.html')
