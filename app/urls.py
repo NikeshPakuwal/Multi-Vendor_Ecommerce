1d5aace42f9ca603885e903592b9df2d76dcab37
NIKESH PAKUWAL <pakuwalnikesh@gmail.com>
2021-06-20 14:22:52 +0545

    Dynamic user profile added

--- app/urls.py
+++ app/urls.py
@@ -35,11 +35,10 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html', form_class=MySetPasswordForm), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
 
-
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     path('checkout/', views.checkout, name='checkout'),
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.http import request
 from django.shortcuts import render
 from django.views import View
 from.models import Product, Cart, OrderPlaced, Customer
-from.forms import CustomerRegistrationForm
+from.forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 
 
@@ -18,15 +18,42 @@ class CustomerRegistrationView(View):
             form.save()
         return render(request, 'app/customerregistration.html', {'form':form})
 
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        context = {
+            'form' : form,
+            'active' : 'btn-primary'
+        }
+        return render(request, 'app/profile.html', context)
+    
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            reg.save()
+
+            messages.success(request, 'Profile Updated Successfully!')
+
+            context = {
+                'form' : form,
+                'active' : 'btn-primary'
+            }
+        return render(request, 'app/profile.html', context)
+
 
 def add_to_cart(request):
      return render(request, 'app/addtocart.html')
 
 def buy_now(request):
     return render(request, 'app/buynow.html')
-
-def profile(request):
-    return render(request, 'app/profile.html')
 
 def address(request):
     return render(request, 'app/address.html')
